3e7207265d8a7e266f4e295f2c1d792b72c60387
MrUnwonted <arjungodzarjun@gmail.com>
2023-08-19 20:44:02 +0530

    Created

--- src/main/java/com/arjun/Spring_JWT/MyUserDetailsService.java
+++ src/main/java/com/arjun/Spring_JWT/MyUserDetailsService.java
@@ -1,12 +1,17 @@
 package com.arjun.Spring_JWT;
 
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+
+@Service
 public class MyUserDetailsService implements UserDetailsService {
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        return new User("arjun", "arjun", new ArrayList<>());
     }
 }
